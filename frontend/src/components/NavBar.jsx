49cc8cffedb14b1945beff8a71a446392f0c3bc3
sophiayau <124006972+sophiayau@users.noreply.github.com>
2024-08-21 14:40:46 -0400

    added buttons for about and login

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -2,12 +2,12 @@ import cunyLogo from "./img/cunyLogo.jpg"
 export default function NavBar(){
 
     return <nav>
-        <div className="container mx-auto flex items-center justify-between">
+        <div className="container mx-auto flex items-center justify-between mt-8">
             <img src={cunyLogo} class="w-32"></img>
-            <h1 className="font-bold flex items-center justify-center h-100 text-3xl text-blue-900">Student Resources</h1>
+            <h1 className="font-bold flex items-center justify-center h-100 text-4xl text-blue-900">Student Resources</h1>
             <div className="flex space-x-4">
-                <h1 className="font-bold text-blue-900">About</h1>
-                <h1 className="font-bold text-blue-900">Login</h1>
+                <button class="bg-white font-bold text-lg text-blue-900 h-10 px-6 rounded-full border-2 border-black hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400">About</button>
+                <button class="bg-white font-bold text-lg text-blue-900 h-10 px-6 rounded-full border-2 border-black hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400">Login</button>
             </div>
         </div>
     </nav>
