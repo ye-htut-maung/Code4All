14a97dc665d1414596d56d9f9d928ecf880cddb0
sophiayau <124006972+sophiayau@users.noreply.github.com>
2024-08-21 14:23:11 -0400

    changed text size of Student Resources

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -4,7 +4,7 @@ export default function NavBar(){
     return <nav>
         <div className="container mx-auto flex items-center justify-between">
             <img src={cunyLogo} class="w-32"></img>
-            <h1 className="font-bold flex items-center justify-center h-80 text-blue-900">Student Resources</h1>
+            <h1 className="font-bold flex items-center justify-center h-100 text-3xl text-blue-900">Student Resources</h1>
             <div className="flex space-x-4">
                 <h1 className="font-bold text-blue-900">About</h1>
                 <h1 className="font-bold text-blue-900">Login</h1>
