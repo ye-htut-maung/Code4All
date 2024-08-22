55a0dab1b5382c82a20ff5d35a9ee371d26e0d71
Jonnathan Saavedra <jonnjstart@gmail.com>
2024-08-21 23:17:42 -0400

    post endpoint for filtered from front

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,23 @@ const pool = new Pool({
 app.use(express.json());
 
 // endpoints for all 3 categories
+
+app.post('/api/resource-details', async (req, res) => {
+  try {
+    const { resourceId } = req.body;
+    
+  //   const result = await pool.query('SELECT * FROM resources WHERE id = $1', [resourceId]);
+  //   if (result.rows.length > 0) {
+  //     res.json(result.rows[0]);
+  //   } else {
+  //     res.status(404).json({ error: 'Resource not found' });
+  //   }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'An error occurred while fetching resource details' });
+  }
+});
+
 app.get('/api/food', async (req, res) => {
     try {
         const result = await pool.query('SELECT * FROM food');
