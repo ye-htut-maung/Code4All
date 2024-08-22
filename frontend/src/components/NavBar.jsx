d894b7561366bcaac2351e59359f5aa401af4e1e
Zian <hasanzian6@gmail.com>
2024-08-22 02:11:27 -0400

    added db endpoint for specific queries

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,13 +1,13 @@
-import cunyLogo from "./img/cunyLogo.jpg"
+//import cunyLogo from "./img/cunyLogo.jpg"
 export default function NavBar(){
 
     return <nav>
         <div className="container mx-auto flex items-center justify-between mt-8">
-            <img src={cunyLogo} class="w-32"></img>
+            
             <h1 className="font-bold flex items-center justify-center h-100 text-4xl text-blue-900">Student Resources</h1>
             <div className="flex space-x-4">
-                <button class="bg-white font-bold text-lg text-blue-900 h-10 px-6 rounded-full border-2 border-black hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400">About</button>
-                <button class="bg-white font-bold text-lg text-blue-900 h-10 px-6 rounded-full border-2 border-black hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400">Login</button>
+                <button className="bg-white font-bold text-lg text-blue-900 h-10 px-6 rounded-full border-2 border-black hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400">About</button>
+                <button className="bg-white font-bold text-lg text-blue-900 h-10 px-6 rounded-full border-2 border-black hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400">Login</button>
             </div>
         </div>
     </nav>
