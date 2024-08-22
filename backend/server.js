a1c3531f7810f70447eb6cd20c68ad765e1375e3
ye-htut-maung <yehtutmaung21@gmail.com>
2024-08-22 15:15:07 -0400

    change port to 3000 and create docker

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
-require('dotenv').config({ path: '../.env' });
-const express = require('express');
-const { Pool} = require('pg');
-const cors = require('cors');
+require("dotenv").config({ path: "../.env" });
+const express = require("express");
+const { Pool } = require("pg");
+const cors = require("cors");
 const app = express();
 
-const port = 5000;
+const port = 3000;
 
 const pool = new Pool({
     user: process.env.DB_USER,
@@ -23,8 +23,7 @@ app.use(express.json());
 //   try {
 //     const { location, resourceType } = req.body;
 //     console.log(location, resourceType);
-    
-    
+
 //   //   const result = await pool.query('SELECT * FROM resources WHERE id = $1', [resourceId]);
 //   //   if (result.rows.length > 0) {
 //   //     res.json(result.rows[0]);
@@ -37,79 +36,75 @@ app.use(express.json());
 //   }
 // });
 
-app.get('/api/food', async (req, res) => {
+app.get("/api/food", async (req, res) => {
     try {
-        const result = await pool.query('SELECT * FROM food');
+        const result = await pool.query("SELECT * FROM food");
         res.json(result.rows);
     } catch (err) {
         console.error(err.stack);
-        res.status(500).send('Error fetching data');
+        res.status(500).send("Error fetching data");
     }
 });
 
-app.get('/api/housing', async (req, res) => {
+app.get("/api/housing", async (req, res) => {
     try {
-        const result = await pool.query('SELECT * FROM housing');
+        const result = await pool.query("SELECT * FROM housing");
         res.json(result.rows);
     } catch (err) {
         console.error(err.stack);
-        res.status(500).send('Error fetching data');
+        res.status(500).send("Error fetching data");
     }
 });
 
-app.get('/api/consultation', async (req, res) => {
+app.get("/api/consultation", async (req, res) => {
     try {
-        const result = await pool.query('SELECT * FROM consultation');
+        const result = await pool.query("SELECT * FROM consultation");
         res.json(result.rows);
     } catch (err) {
         console.error(err.stack);
-        res.status(500).send('Error fetching data');
+        res.status(500).send("Error fetching data");
     }
 });
 
-app.post('/api/resource-details', async (req, res) => {
+app.post("/api/resource-details", async (req, res) => {
     const { boroughs, type } = req.body;
     console.log(boroughs);
     console.log(type);
 
     if (!boroughs || !type) {
-        return res.status(400).send('Both boroughs and type are required');
+        return res.status(400).send("Both boroughs and type are required");
     }
 
     try {
-        let query = '';
+        let query = "";
         let params = [];
 
         switch (type.toLowerCase()) {
-            case 'food':
-                query = 'SELECT * FROM food WHERE boroughs = $1';
+            case "food":
+                query = "SELECT * FROM food WHERE boroughs = $1";
                 params = [boroughs];
                 break;
-            case 'housing':
-                query = 'SELECT * FROM housing WHERE boroughs = $1';
+            case "housing":
+                query = "SELECT * FROM housing WHERE boroughs = $1";
                 params = [boroughs];
                 break;
-            case 'consultation':
-                query = 'SELECT * FROM consultation WHERE boroughs = $1';
+            case "consultation":
+                query = "SELECT * FROM consultation WHERE boroughs = $1";
                 params = [boroughs];
                 break;
             default:
-                return res.status(400).send('Invalid type');
+                return res.status(400).send("Invalid type");
         }
-        
-        
+
         const result = await pool.query(query, params);
         res.json(result.rows);
         console.log(result.rows);
-
     } catch (err) {
         console.error(err.stack);
-        res.status(500).send('Error fetching data');
+        res.status(500).send("Error fetching data");
     }
 });
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
-
-  
