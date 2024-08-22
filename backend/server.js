da17d2766385efee991b6e8beda74ecdae3974e3
Jshot117 <jonnjstart@gmail.com>
2024-08-22 16:26:20 -0400

    fixed pathing for env

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: "../.env" });
+require("dotenv").config({ path: "./.env" });
 const express = require("express");
 const { Pool } = require("pg");
 const cors = require("cors");
